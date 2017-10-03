82e15b79408c8543ebae330fe9c55a13979f2c8b
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-10-03 15:17:50 -0500

    added map component

---
+++ src/components/map.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Map extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            active_cam: 1,
+        }
+    }
+}
