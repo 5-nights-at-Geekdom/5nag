e046d5bc13e36d255cce2dd269478e877c0f59d6
Adam Age <adamage@Adams-MacBook-Pro.local>
2017-10-10 18:56:08 -0500

    Updated menu to have routes to corresponding views. New game goes to the game, Continue goes to game component but we can pass it a state. Options goes to the options page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Menu from './components/menu';
+import Options from './components/options'
 import Game from './components/game';
 
 
 const App = () => {
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to 5NAG</h1>
-        </header>
-        <Menu />
-        <Game />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact={true} path="/" component={ Menu } />
+          <Route path="/options" component={ Options }/>
+          <Route path="/game" component={ Game } />
+        </div>
+      </Router>
     );
 }
 
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const menu = () => {
   return (
     <div>
-      <ul>
-        <li>Start Game</li>
-        <li>Options</li>
-      </ul>
+      <div className="menu__box">
+        <h2> Five Nights at Geekdom </h2>
+        <ul>
+          <li>
+            <Link to={'/game'}>New Game</Link>
+          </li>
+          <li>
+            <Link to={{pathname: '/game', state: 'test'}}>Continue</Link>
+          </li>
+          <li>
+            <Link to={'/options'}>Options</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
