d122501a85f5afc366bc8b58103d5193caa7ee78
Cody <cody@hastings.email>
2017-10-23 18:50:10 -0500

    popup now works, added logic so enemy will not pop up on a cam you are looking at

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import $ from 'jquery'
 
 
 class Pop_Up extends Component {
@@ -6,9 +7,16 @@ class Pop_Up extends Component {
         super(props)
     }
 
+    handleClick(){
+    	$("#popUp").hide()
+    }
+
     render(){
     	return (
-          	<div id="popUp" className="popUpView"></div>
+          	<div id="popUp" className="popUpView">
+          		<div className="littleX" onClick={() => this.handleClick()}></div>
+          		<div className="clickMe" ></div>
+          	</div>
     	)
     }
 }
