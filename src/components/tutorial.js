d166c8ce7e2dbd328a9ba622357fd973805003da
Cody <cody@hastings.email>
2017-10-31 09:55:06 -0500

    added tutorial component

---
+++ src/components/tutorial.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+class Tutorial extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+      return (
+            <div id="tutorial"></div>
+      )
+    }
+}
+
+export default Tutorial
